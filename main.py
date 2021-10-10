cd4400aff51d0b0a897ca0bb490b76eee7c97298
“Maja <majaa.hladnik@gmail.com>
2021-10-10 20:23:14 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+mood = input("Mood? ")
+if mood == "Happy":
+    print("It's great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Don't worry, everything will be fine.")
+elif mood == "excited":
+    print("Good for you!")
+elif mood == "Relaxed":
+    print("Enjoy ;)")
+else:
+    print("I don't recognize this mood.")
